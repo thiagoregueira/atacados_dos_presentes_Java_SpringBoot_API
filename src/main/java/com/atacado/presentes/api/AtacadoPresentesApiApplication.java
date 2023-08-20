2fd35d59df879adb668d5d0021ee67d5cb437e79
alexandresjunior <souzajr.alexandre@gmail.com>
2023-08-19 21:51:42 -0300

    APR-001 Criação do projeto com Spring Boot

---
+++ src/main/java/com/atacado/presentes/api/AtacadoPresentesApiApplication.java
@@ -0,0 +1,13 @@
+package com.atacado.presentes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtacadoPresentesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtacadoPresentesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atacado/presentes/api/AtacadoPresentesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atacado.presentes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtacadoPresentesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
