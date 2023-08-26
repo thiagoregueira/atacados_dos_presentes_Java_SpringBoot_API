1debe95203314cc214652b4b42036b617819844b
Pedro-melo12 <pedrinhomelo2005@gmail.com>
2023-08-26 16:09:31 -0300

    APR-013 Associação da entidade produtos com fornecedor e categorias

--- src/main/java/com/atacado/presentes/api/model/Produto.java
+++ src/main/java/com/atacado/presentes/api/model/Produto.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.DecimalMin;
@@ -39,7 +40,7 @@ public class Produto {
     private Fornecedor fornecedor;
 
     @ManyToMany
-    @JoinColumn(name = "idCategoria")
+    @JoinTable(name = "tb_produtos_categorias", joinColumns = {@JoinColumn(name = "idProduto")}, inverseJoinColumns = {@JoinColumn(name = "idCategoria")})
     private List<Categoria> categorias;
 
 }
--- src/main/java/com/atacado/presentes/api/repository/ProdutoRepository.java
+++ src/main/java/com/atacado/presentes/api/repository/ProdutoRepository.java
@@ -1,9 +1,16 @@
 package com.atacado.presentes.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.atacado.presentes.api.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    @Query("SELECT p FROM tb_produtos p JOIN p.categorias c, tb_avaliacoes_produto ap WHERE c.idCategoria = :idCategoria ORDER BY ap.pontuacao DESC")
+    List<Produto>findByProdutosPorCategorias(@Param("idCategoria") Long idCategoria);
 
 }
