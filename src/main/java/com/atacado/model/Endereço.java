82ad079b3b599e25ddcf3a9626098aee439f4690
Pedro-melo12 <pedrinhomelo2005@gmail.com>
2023-08-26 11:41:37 -0300

    APR-002 Adição de getters e setters,formatação do código e alteração no regex

--- src/main/java/com/atacado/model/Endereço.java
+++ src/main/java/com/atacado/model/Endereço.java
@@ -4,27 +4,40 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Endereço {
 
     @Column(nullable = true, length = 10)
-    @JsonFormat(pattern = "XX.XXX-XX")
+    @JsonFormat(pattern = "\\\\d{5}-\\\\d{3}")
     private String cep;
+
     @Column(nullable = true, length = 255)
     private String logradouro;
+
     @Column(nullable = true, length = 10)
     private String numero;
+
     @Column(nullable = true, length = 50)
     private String complemento;
+
     @Column(nullable = true, length = 50)
     private String bairro;
+
     @Column(nullable = true, length = 50)
     private String cidade;
+
     @Column(nullable = true, length = 2)
     private String uf;
+
     @Column(nullable = true, length = 255)
     private String pontoDeReferencia;
 
 }
+
