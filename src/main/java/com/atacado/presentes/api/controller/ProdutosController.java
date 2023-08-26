526eba38e22e96ba82ce5a1f5c96db8c4cb700eb
Daniela Xavier <danielamxsoares@gmail.com>
2023-08-26 13:02:11 -0300

    APR-006 criar padrão ResponseEntity

--- src/main/java/com/atacado/presentes/api/controller/ProdutosController.java
+++ src/main/java/com/atacado/presentes/api/controller/ProdutosController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,22 +25,33 @@ public class ProdutosController {
     ProdutosRepository produtosRepository;
 
     @PostMapping
-    public Produtos cadastrarProduto (@RequestBody Produtos produtos) {
-        return produtosRepository.save(produtos);
+    public ResponseEntity<Produtos> cadastrarProduto (@RequestBody Produtos produtos) {
+
+        Produtos produto = produtosRepository.save(produtos);
+
+        return ResponseEntity.ok().body(produto);
     }
 
     @GetMapping
-    public List<Produtos> listarProdutos() {
-        return produtosRepository.findAll();
+    public ResponseEntity<List<Produtos>> listarProdutos() {
+
+        List<Produtos> produtosList = produtosRepository.findAll();
+
+        return ResponseEntity.ok().body(produtosList);
+        
     }
 
     @GetMapping("/{id}")
-    public Optional<Produtos> listarProdutosPorId(@PathVariable("id") Long id) {
-        return produtosRepository.findById(id);
+    public ResponseEntity<Optional<Produtos>> listarProdutosPorId(@PathVariable("id") Long id) {
+
+        Optional<Produtos> produto = produtosRepository.findById(id);
+
+        return ResponseEntity.ok().body(produto);
     }
 
     @PutMapping("/{id}")
-    public Produtos atualizarProduto (@PathVariable("id") Long id, @RequestBody Produtos produtos) {
+    public ResponseEntity<Produtos> atualizarProduto (@PathVariable("id") Long id, @RequestBody Produtos produtos) {
+        
         Optional<Produtos> produtoExistente = produtosRepository.findById(id);
 
         if(produtoExistente.isPresent()) {
@@ -47,17 +59,19 @@ public class ProdutosController {
             produtoExistente.get().setPreco(produtos.getPreco());
             produtoExistente.get().setDescricao(produtos.getDescricao());
 
-            return produtosRepository.save(produtoExistente.get());
+            Produtos produto = produtosRepository.save(produtoExistente.get());
+
+            return ResponseEntity.ok().body(produto);
         }
 
         return null;
     }
     
     @DeleteMapping("/{id}")
-    public String deletarProduto(@PathVariable("id") Long id) {
+    public ResponseEntity<Produtos> deletarProduto(@PathVariable("id") Long id) {
         
         produtosRepository.deleteById(id);
 
-        return "Produto deletado com sucesso!";
+        return ResponseEntity.noContent().build();
     }
 }
