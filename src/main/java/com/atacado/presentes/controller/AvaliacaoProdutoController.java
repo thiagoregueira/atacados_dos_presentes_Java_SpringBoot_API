cf543914702e39421eba09c363fa71047daeecd8
unknown <igorrx24072004@gmail.com>
2023-08-26 11:23:55 -0300

    [APR-007] Read and Delete de Avaliações do Produto

--- src/main/java/com/atacado/presentes/controller/AvaliacaoProdutoController.java
+++ src/main/java/com/atacado/presentes/controller/AvaliacaoProdutoController.java
@@ -3,6 +3,8 @@ package com.atacado.presentes.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +24,13 @@ public class AvaliacaoProdutoController {
         return avaliacaoProdutoRepository.save(avaliacaoproduto);
     }
 
+
+    @GetMapping("/{idAvaliacao}")
+    public Optional<AvaliacaoProduto> buscarAvaliacaoPeloId(@PathVariable("id") Long id) {
+        return avaliacaoProdutoRepository.findById(id);
+    }
+
+
     @PutMapping("/{id}")
     public AvaliacaoProduto atualizarAvaliacaoProduto(
             @PathVariable("id") Long id,
@@ -31,10 +40,19 @@ public class AvaliacaoProdutoController {
         if (avaliacaoProdutoExistente.isPresent()) {
             avaliacaoProdutoExistente.get().setPontuacao(avaliacaoProduto.getPontuacao());
             avaliacaoProdutoExistente.get().setComentario(avaliacaoProduto.getComentario());
-            
-
             return avaliacaoProdutoRepository.save(avaliacaoProdutoExistente.get());
         }
+        
+        return null; 
+    }
+
+
+    @DeleteMapping("/{id}")
+    public String deletarAvaliacaoPeloId(@PathVariable("id") Long id) {
+        avaliacaoProdutoRepository.deleteById(id);
+
+        return "Avaliação deletada com sucesso!";
+    }
 
 
     @Autowired
