2fb0fca71dbaea429374b8781443ac2cc4458b15
marcsp-7 <kynho03@gmail.com>
2023-08-26 13:13:46 -0300

    APR-005 Criado o repository e o controller

---
+++ src/main/java/com/atacado/presentes/api/controller/FornecedorController.java
@@ -0,0 +1,77 @@
+package com.atacado.presentes.api.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atacado.presentes.api.model.Fornecedor;
+import com.atacado.presentes.api.repository.FornecedorRepository;
+
+@RestController
+@RequestMapping(value = "/fornecedores")
+public class FornecedorController {
+
+    @PostMapping
+    public ResponseEntity<Fornecedor> cadastrarFornecedor(@RequestBody Fornecedor fornecedores) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(fornecedorRepository.save(fornecedores));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Fornecedor>> listarFornecedores(Fornecedor fornecedor) {
+        return ResponseEntity.status(HttpStatus.OK).body(fornecedorRepository.findAll());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Fornecedor> buscarFornecedorPeloId(@PathVariable("id") Long id) {
+        Optional<Fornecedor> fornecedor = fornecedorRepository.findById(id);
+        if (fornecedor.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(fornecedor.get());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Fornecedor> atualizarFornecedor(@PathVariable("id") Long id,
+            @RequestBody Fornecedor fornecedor) {
+        Optional<Fornecedor> fornecedorExistente = fornecedorRepository.findById(id);
+
+        if (fornecedorExistente.isPresent()) {
+            fornecedorExistente.get().setNomeFantasia(fornecedor.getNomeFantasia());
+            fornecedorExistente.get().setRazaoSocial(fornecedor.getRazaoSocial());
+            fornecedorExistente.get().setCnpj(fornecedor.getCnpj());
+
+            return ResponseEntity.status(HttpStatus.OK).body(fornecedorRepository.save(fornecedorExistente.get()));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletarFornecedorPeloId(@PathVariable("id") Long id) {
+        Optional<Fornecedor> fornecedor = fornecedorRepository.findById(id);
+
+        if (fornecedor.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        fornecedorRepository.deleteById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Fornecedor deletado com sucesso!");
+    }
+
+    @Autowired
+    private FornecedorRepository fornecedorRepository;
+}
---
+++ src/main/java/com/atacado/presentes/api/model/Fornecedor.java
@@ -0,0 +1,37 @@
+package com.atacado.presentes.api.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_fornecedores")
+
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idFornecedores;
+
+    @Column(nullable = false, length = 255)
+    private String nomeFantasia;
+
+    @Column(nullable = false, length = 255)
+    private String razaoSocial;
+
+    @Column(nullable = false, length = 18)
+    @JsonFormat(pattern = "\\d{2}.\\d{3}.\\d{3}/0001-\\d{2}")
+    private String cnpj;
+
+    
+
+}
