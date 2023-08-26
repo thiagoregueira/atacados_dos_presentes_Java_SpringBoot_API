8a22446b04efc26a76e43b50a8448aad29e04286
alexandresjunior <souzajr.alexandre@gmail.com>
2023-08-26 16:08:04 -0300

    Correção do import do Id

--- src/main/java/com/atacado/presentes/api/model/AvaliacaoProduto.java
+++ src/main/java/com/atacado/presentes/api/model/AvaliacaoProduto.java
@@ -1,11 +1,10 @@
 package com.atacado.presentes.api.model;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
