bcbad8dadf8e3e7729d85d5cc3a2d7e652a467bc
guilhermeviturino <guilhermevitturino@gmail.com>
2023-08-26 10:30:58 -0300

    apr-004 criação do model

---
+++ src/main/java/com/atacado/model/Cliente.java
@@ -0,0 +1,35 @@
+package com.atacado.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_clientes")
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCliente;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, length = 255)
+    private String sobrenome;
+    
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyy")
+    private LocalDate dataNascimento;
+}
