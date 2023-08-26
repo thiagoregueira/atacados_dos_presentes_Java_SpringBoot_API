65e624584ab34158701b8010febf0518c36f7b86
Daniela Xavier <danielamxsoares@gmail.com>
2023-08-26 11:04:50 -0300

    APR-006 corrigir repository

--- src/main/java/com/atacado/presentes/api/repository/ProdutosRepository.java
+++ src/main/java/com/atacado/presentes/api/repository/ProdutosRepository.java
@@ -1,8 +1,10 @@
 package com.atacado.presentes.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.atacado.presentes.api.model.Produtos;
+import org.springframework.stereotype.Repository;
 
+import com.atacado.presentes.api.model.Produtos;
+@Repository
 public interface ProdutosRepository extends JpaRepository<Produtos, Long>{
     
 }
