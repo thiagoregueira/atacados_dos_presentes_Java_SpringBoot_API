a311c2803f6da9572aa045aa9e664a0b1326affb
guilhermeviturino <guilhermevitturino@gmail.com>
2023-08-26 10:33:59 -0300

    apr-004 criacao do repository

---
+++ src/main/java/com/atacado/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.atacado.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.atacado.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+    
+}
