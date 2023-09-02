4ba84eb536d3458969369de1e8696fba65bdb185
Guilherme Vituino <guilhermevitturino@gmail.com>
2023-09-02 14:46:08 -0300

    apr-018 criacao da tabela fornecedor no migration

---
+++ src/main/resources/db/migration/V4_CREATE-TABLE-FORNECEDOR.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tb_fonecedores(
+    idFornecedor BIGINT NOT NULL AUTO_INCREMENT,
+    nomeFantasia VARCHAR(255) NOT NULL,
+    razaoSocial VARCHAR(255) NOT NULL,
+    cnpj VARCHAR(18) NOT NULL,
+    PRIMARY KEY(idFornecedor),
+    FOREIGN KEY (idUsuario) REFERENES tb_usuarios(idUsuario)
+);
