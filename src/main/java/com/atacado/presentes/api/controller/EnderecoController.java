d915fb079da279fc3b0afad91df246d6788a771d
thiagoregueira <thiago.regueira@yahoo.com.br>
2023-09-02 14:25:02 -0300

    APR-021 criacao da classe EnderecoController com a rota retornando o endereco após consumir a api via cep.

---
+++ src/main/java/com/atacado/presentes/api/controller/EnderecoController.java
@@ -0,0 +1,37 @@
+package com.atacado.presentes.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.atacado.presentes.api.model.Endereco;
+import com.atacado.presentes.dto.EnderecoDto;
+
+@RestController
+@RequestMapping("/enderecos")
+public class EnderecoController {
+
+    @PostMapping("")
+    public Endereco getEndereco(@RequestBody EnderecoDto enderecoDto) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        EnderecoDto resposta = restTemplate.getForObject("https://viacep.com.br/ws/" + enderecoDto.getCep() + "/json",
+                EnderecoDto.class);
+
+        if (resposta != null) {
+
+            Endereco endereco = new Endereco();
+            endereco.setCep(resposta.getCep());
+            endereco.setLogradouro(resposta.getLogradouro());
+            endereco.setComplemento(resposta.getComplemento());
+            endereco.setBairro(resposta.getBairro());
+            endereco.setCidade(resposta.getLocalidade());
+            endereco.setUf(resposta.getUf());
+
+            return endereco;
+        }
+        return null;
+    }
+}
