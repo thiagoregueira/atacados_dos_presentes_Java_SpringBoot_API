6645f7e8a49173b89181869c063704e97dee16f2
Pedro-melo12 <pedrinhomelo2005@gmail.com>
2023-09-02 13:56:17 -0300

    APR-025 alteração do ok para no content

--- src/main/java/com/atacado/presentes/api/controller/UsuarioController.java
+++ src/main/java/com/atacado/presentes/api/controller/UsuarioController.java
@@ -59,7 +59,7 @@ public class UsuarioController {
             return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuarioExistente.get()));
         }
 
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping("/{id}")
